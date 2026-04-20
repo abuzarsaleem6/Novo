d9db9a6c4aefa201564627fd8e86e4633ea3c555
Abdullah Shah <syedmuhammadAbdullahShah64@gmail.com>
2026-04-20 17:49:37 +0500

    Implementing Timer.cpp

---
+++ Timer.cpp
@@ -0,0 +1,39 @@
+#include "Timer.h"
+
+
+Timer::Timer() {
+    isRunning = false;
+}
+
+Timer::~Timer() {
+    
+}
+
+
+void Timer::startSession() {
+   
+    startTime = std::chrono::steady_clock::now();
+    isRunning = true;
+}
+
+double Timer::getSessionDuration() const {
+    if (!isRunning) {
+        return 0.0;
+    }
+
+    
+    auto currentTime = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsedSeconds = currentTime - startTime;
+
+    return elapsedSeconds.count();
+}
+
+void Timer::displaySessionTime() const {
+    double totalSeconds = getSessionDuration();
+
+   
+    int minutes = static_cast<int>(totalSeconds) / 60;
+    int seconds = static_cast<int>(totalSeconds) % 60;
+
+    std::cout << "Active Session Time: " << minutes << "m " << seconds << "s\n";
+}
