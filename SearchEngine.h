3f88b7b15d82caf363110a203918a9d45e38da13
DarkCircuitCipher <l252022@lhr.nu.edu.pk>
2026-04-20 18:41:46 +0500

    partail searching before clicking search button

--- SearchEngine.h
+++ SearchEngine.h
@@ -14,6 +14,8 @@ public:
 	void addUser(User* user);
 	User* searchUsers(const QString& username) const;
 	void displayResults(const QString& username) const;
+
+	QList<User*> searchPartial(const QString& query) const;
 };
 
 #endif
