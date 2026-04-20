4f2fe685058f48edd4e0f91e2a7a3301b3913a13
Abdullah Shah <syedmuhammadAbdullahShah64@gmail.com>
2026-04-20 17:43:08 +0500

    Drafting Timer.h

--- Timer.h
+++ Timer.h
@@ -1,1 +1,20 @@
 #pragma once
+#include <iostream>
+#include <chrono>
+
+class Timer {
+private:
+   
+    std::chrono::time_point<std::chrono::steady_clock> startTime;
+    bool isRunning;
+
+public:
+   
+    Timer();
+    ~Timer(); 
+
+  
+    void startSession();
+    double getSessionDuration() const;
+    void displaySessionTime() const;  
+};
