e13fc483f703c552ee54892f2303a0b099224d4c
DarkCircuitCipher <l252022@lhr.nu.edu.pk>
2026-04-21 23:03:39 +0500

    admin in process

---
+++ Admin.cpp
@@ -0,0 +1,94 @@
+#include"Admin.h"
+
+Admin::Admin() : User() {
+	adminLevel = "Super";
+	reportedUserCount = 0;
+	reportedPostCount = 0;
+}
+
+Admin::Admin(QString username, QString password) : User(username.toStdString(), password.toStdString()) {
+	adminLevel = "Super";
+	reportedUserCount = 0;
+	reportedPostCount = 0;
+}
+
+void Admin::deleteUser(User**& allUsers, int& userCount, QString username) {
+	for (int i = 0; i < userCount; i++) {
+		if (QString::fromStdString(allUsers[i]->getUsername()) == username) {
+			allUsers[i]->deleteAccount(allUsers, userCount);
+			break;
+		}
+	}
+}
+
+void Admin::deletePost(User** allUsers, int userCount, QString postId) {
+	for (int i = 0; i < userCount; i++) {
+		Posts* post = allUsers[i]->getPostById(postId.toStdString());
+		if (post != nullptr) {
+			post->~Posts();
+			break;
+		}
+	}
+}
+
+void Admin::deleteComment(Posts* post, int commentIndex) {
+	QList<Comment> comments = post->getComments();
+	if (commentIndex >= 0 && commentIndex < comments.size()) {
+		comments.removeAt(commentIndex);
+		post->saveCommentsToFile();
+	}
+}
+
+void Admin::reviewReports(User**& allUsers, int& userCount) {
+	qDebug() << "Reviewing reported users and posts...";
+	for(int i = 0; i < reportedUserCount; i++) {
+		User* user = reportedUsers[i];
+		qDebug() << "User: " << QString::fromStdString(user->getUsername()) << " has been reported " << user->getIsReported() << " times.";
+	}
+	/*for(int i = 0; i < reportedPostCount; i++) {
+		Posts* post = reportedPosts[i];
+		qDebug() << "Post: " << QString::fromStdString(post->getPostId()) << " has been reported " << post->getIsReported() << " times.";
+	}*/
+}
+
+Admin::~Admin() {
+	
+}
+
+void Admin::displayAdminDashboard() {
+	
+}
+
+void Admin::displayReportedUsers() {
+
+}
+
+void Admin::displayReportedPosts() {
+	
+}
+
+void Admin::receiveReport(User* user) {
+	reportedUsers.append(user);
+	reportedUserCount++;
+}
+
+Admin::Admin(const Admin& other) : User(other) {
+	adminLevel = other.adminLevel;
+	reportedUsers = other.reportedUsers;
+	reportedPosts = other.reportedPosts;
+	reportedUserCount = other.reportedUserCount;
+	reportedPostCount = other.reportedPostCount;
+}
+
+Admin& Admin::operator=(const Admin& other) {
+	if (this != &other) {
+		User::operator=(other);
+		adminLevel = other.adminLevel;
+		reportedUsers = other.reportedUsers;
+		reportedPosts = other.reportedPosts;
+		reportedUserCount = other.reportedUserCount;
+		reportedPostCount = other.reportedPostCount;
+	}
+	return *this;
+}
+
--- Admin.h
+++ Admin.h
@@ -1,1 +1,34 @@
 #pragma once
+#ifndef ADMIN_H
+#define ADMIN_H
+
+#include "User.h"
+#include "Post.h"
+
+class Admin :public User {
+private:
+	QString adminLevel;
+	QList<User*> reportedUsers;
+	QList<Posts*> reportedPosts;
+	int reportedUserCount;
+	int reportedPostCount;
+public:
+	Admin();
+	Admin(QString username, QString password);
+	void deleteUser(User**& allUsers, int& userCount, QString username);
+	void deletePost(User** allUsers, int userCount, QString postId);
+	void deleteComment(Posts* post, int commentIndex);
+	void reviewReports(User**& allUsers, int& userCount);
+	~Admin();
+	void displayAdminDashboard();
+	void displayReportedUsers();
+	void displayReportedPosts();
+	void receiveReport(User* user);
+	Admin(const Admin& other);
+	Admin& operator=(const Admin& other);
+
+};
+
+
+
+#endif
