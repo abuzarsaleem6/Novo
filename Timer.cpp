bed86e9dec6ab26ac33dbfa10c053136bd837d93
Abdullah Shah <syedmuhammadAbdullahShah64@gmail.com>
2026-05-01 15:45:00 +0500

    V56

--- Timer.cpp
+++ Timer.cpp
@@ -1,57 +1,114 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "Timer.h"
-#include <iostream>
-#include <fstream>
 
 using namespace std;
 
-Timer::Timer() {
+
+void Timer::allocateAndCopy(char*& dest, const char* source) {
+    if (source != nullptr) {
+        dest = new char[strlen(source) + 1];
+        strcpy(dest, source);
+    }
+    else {
+        dest = new char[1];
+        dest[0] = '\0';
+    }
+}
+
+
+Timer::Timer(const char* username) {
+    ownerUsername = nullptr;
+    allocateAndCopy(ownerUsername, username);
+    startHour = 0;
+    startMinute = 0;
+    startSecond = 0;
+    elapsedSeconds = 0;
     isRunning = false;
 }
 
+Timer::Timer(const Timer& other) {
+    ownerUsername = nullptr;
+    allocateAndCopy(ownerUsername, other.ownerUsername);
+    startHour = other.startHour;
+    startMinute = other.startMinute;
+    startSecond = other.startSecond;
+    elapsedSeconds = other.elapsedSeconds;
+    isRunning = other.isRunning;
+}
+
+Timer& Timer::operator=(const Timer& other) {
+    if (this != &other) {
+        delete[] ownerUsername;
+        allocateAndCopy(ownerUsername, other.ownerUsername);
+        startHour = other.startHour;
+        startMinute = other.startMinute;
+        startSecond = other.startSecond;
+        elapsedSeconds = other.elapsedSeconds;
+        isRunning = other.isRunning;
+    }
+    return *this;
+}
+
 Timer::~Timer() {
-
+    delete[] ownerUsername;
 }
 
-void Timer::startSession() {
-    startTime = chrono::steady_clock::now();
-    isRunning = true;
-}
-
-double Timer::getSessionDuration() const {
+/
+void Timer::start() {
     if (!isRunning) {
-        return 0.0;
+        time_t now = time(0);
+        tm* ltm = localtime(&now);
+        startHour = ltm->tm_hour;
+        startMinute = ltm->tm_min;
+        startSecond = ltm->tm_sec;
+        isRunning = true;
     }
-
-    auto currentTime = chrono::steady_clock::now();
-    chrono::duration<double> elapsedSeconds = currentTime - startTime;
-
-    return elapsedSeconds.count();
 }
 
-void Timer::displaySessionTime() const {
-    double totalSeconds = getSessionDuration();
-
-    int minutes = static_cast<int>(totalSeconds) / 60;
-    int seconds = static_cast<int>(totalSeconds) % 60;
-
-    cout << "Active Session Time: " << minutes << "m " << seconds << "s\n";
+void Timer::stop() {
+    isRunning = false;
+    update(); 
 }
 
-void Timer::saveSessionLog(const char* username) const {
-    ofstream outFile("session_logs.txt", ios::app);
+void Timer::reset() {
+    elapsedSeconds = 0;
+    isRunning = false;
+}
 
-    if (outFile.is_open()) {
-        double totalSeconds = getSessionDuration();
+void Timer::update() {
+    if (isRunning) {
+        time_t now = time(0);
+        tm* ltm = localtime(&now);
 
-        int minutes = static_cast<int>(totalSeconds) / 60;
-        int seconds = static_cast<int>(totalSeconds) % 60;
+        int currentHour = ltm->tm_hour;
+        int currentMinute = ltm->tm_min;
+        int currentSecond = ltm->tm_sec;
 
-        outFile << "User: " << username << " | Session Time: " << minutes << "m " << seconds << "s\n";
+      
+        int startInSeconds = startHour * 3600 + startMinute * 60 + startSecond;
+        int currentInSeconds = currentHour * 3600 + currentMinute * 60 + currentSecond;
 
-        outFile.close();
+      
+        if (currentInSeconds < startInSeconds) {
+            currentInSeconds += 24 * 3600;
+        }
+
+        elapsedSeconds = currentInSeconds - startInSeconds;
     }
-    else {
-        cout << "Error: Could not open session_logs.txt to save the timer.\n";
-    }
+}
+
+
+void Timer::displayTimeSpent() const {
+    int minutes = elapsedSeconds / 60;
+    int seconds = elapsedSeconds % 60;
+    cout << "Session Time for " << (ownerUsername ? ownerUsername : "Unknown")
+        << ": " << minutes << "m " << seconds << "s\n";
+}
+
+int Timer::getElapsedSeconds() const {
+    return elapsedSeconds;
+}
+
+bool Timer::getIsRunning() const {
+    return isRunning;
 }
