5ba9ba2fc7c7e53c34f255c8a10167f6564b7701
Abdullah Shah <syedmuhammadAbdullahShah64@gmail.com>
2026-05-01 15:37:34 +0500

    V56

--- Message.cpp
+++ Message.cpp
@@ -1,105 +1,133 @@
-#define _CRT_SECURE_NO_WARNINGS 
+#define _CRT_SECURE_NO_WARNINGS
 #include "Message.h"
+#include <ctime>
 
-using namespace std; 
+using namespace std;
+
+
+void Message::allocateAndCopy(char*& dest, const char* source) {
+    if (source != nullptr) {
+        dest = new char[strlen(source) + 1];
+        strcpy(dest, source);
+    }
+    else {
+        dest = new char[1];
+        dest[0] = '\0';
+    }
+}
+
 
 Message::Message() {
     content = nullptr;
-    senderUsername = "System";
-    receiverUsername = "System";
+    senderUsername = nullptr;
+    receiverUsername = nullptr;
+    timestamp = nullptr;
+
+    allocateAndCopy(content, "");
+    allocateAndCopy(senderUsername, "System");
+    allocateAndCopy(receiverUsername, "System");
+    allocateAndCopy(timestamp, "");
+
+    isRead = false;
+    generateTimestamp();
 }
 
-Message::Message(const char* text, string sender, string receiver) {
-    senderUsername = sender;
-    receiverUsername = receiver;
+Message::Message(const char* text, const char* sender, const char* receiver) {
+    content = nullptr;
+    senderUsername = nullptr;
+    receiverUsername = nullptr;
+    timestamp = nullptr;
 
-    if (text != nullptr) {
-        content = new char[strlen(text) + 1];
-        strcpy(content, text);
+    allocateAndCopy(content, text);
+    allocateAndCopy(senderUsername, sender);
+    allocateAndCopy(receiverUsername, receiver);
+
+    isRead = false;
+    generateTimestamp();
+}
+
+Message::Message(const Message& source) {
+    content = nullptr;
+    senderUsername = nullptr;
+    receiverUsername = nullptr;
+    timestamp = nullptr;
+
+    allocateAndCopy(content, source.content);
+    allocateAndCopy(senderUsername, source.senderUsername);
+    allocateAndCopy(receiverUsername, source.receiverUsername);
+    allocateAndCopy(timestamp, source.timestamp);
+
+    isRead = source.isRead;
+}
+
+Message& Message::operator=(const Message& source) {
+    if (this != &source) {
+        delete[] content;
+        delete[] senderUsername;
+        delete[] receiverUsername;
+        delete[] timestamp;
+
+        allocateAndCopy(content, source.content);
+        allocateAndCopy(senderUsername, source.senderUsername);
+        allocateAndCopy(receiverUsername, source.receiverUsername);
+        allocateAndCopy(timestamp, source.timestamp);
+
+        isRead = source.isRead;
     }
-    else {
-        content = nullptr;
-    }
+    return *this;
 }
 
 Message::~Message() {
     delete[] content;
+    delete[] senderUsername;
+    delete[] receiverUsername;
+    delete[] timestamp;
 }
 
-Message::Message(const Message& source) {
-    senderUsername = source.senderUsername;
-    receiverUsername = source.receiverUsername;
-
-    if (source.content != nullptr) {
-        content = new char[strlen(source.content) + 1];
-        strcpy(content, source.content);
-    }
-    else {
-        content = nullptr;
-    }
-}
-
-Message& Message::operator=(const Message& source) {
-    if (this == &source) {
-        return *this;
-    }
-
-    delete[] content;
-
-    senderUsername = source.senderUsername;
-    receiverUsername = source.receiverUsername;
-
-    if (source.content != nullptr) {
-        content = new char[strlen(source.content) + 1];
-        strcpy(content, source.content);
-    }
-    else {
-        content = nullptr;
-    }
-
-    return *this;
-}
 
 const char* Message::getContent() const { return content; }
-string Message::getSenderUsername() const { return senderUsername; }
-string Message::getReceiverUsername() const { return receiverUsername; }
+const char* Message::getSenderUsername() const { return senderUsername; }
+const char* Message::getReceiverUsername() const { return receiverUsername; }
+const char* Message::getTimestamp() const { return timestamp; }
+bool Message::getIsRead() const { return isRead; }
+
 
 void Message::setContent(const char* text) {
     delete[] content;
-
-    if (text != nullptr) {
-        content = new char[strlen(text) + 1];
-        strcpy(content, text);
-    }
-    else {
-        content = nullptr;
-    }
+    allocateAndCopy(content, text);
 }
 
-void Message::setSenderUsername(string sender) { senderUsername = sender; }
-void Message::setReceiverUsername(string receiver) { receiverUsername = receiver; }
+void Message::setSenderUsername(const char* sender) {
+    delete[] senderUsername;
+    allocateAndCopy(senderUsername, sender);
+}
+
+void Message::setReceiverUsername(const char* receiver) {
+    delete[] receiverUsername;
+    allocateAndCopy(receiverUsername, receiver);
+}
+
+void Message::markAsRead() {
+    isRead = true;
+}
+
+
+void Message::generateTimestamp() {
+    time_t now = time(0);
+    tm* ltm = localtime(&now);
+    char buffer[20];
+    
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", ltm);
+
+    if (timestamp) {
+        delete[] timestamp;
+    }
+    allocateAndCopy(timestamp, buffer);
+}
+
 
 void Message::saveToFile(ofstream& out) const {
-    out << senderUsername << " " << receiverUsername << "\n";
-    if (content != nullptr) {
-        out << content << "\n";
-    }
-    else {
-        out << "NULL\n";
-    }
-}
-
-void Message::loadFromStream(ifstream& in) {
-    in >> senderUsername >> receiverUsername;
-    in.ignore();
-
-    char tempBuffer[1024];
-    in.getline(tempBuffer, 1024);
-
-    if (strcmp(tempBuffer, "NULL") != 0) {
-        setContent(tempBuffer);
-    }
-    else {
-        setContent(nullptr);
+    if (senderUsername && receiverUsername && timestamp && content) {
+        out << senderUsername << "|" << receiverUsername << "|" << timestamp << "|" << content << "\n";
     }
 }
